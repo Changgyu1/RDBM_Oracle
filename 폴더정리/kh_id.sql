42bb5ecf1ac0a7da5ce91a9f60c5cd4206416cc6
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-10-16 09:22:12 +0900

    a

---
+++ 폴더정리/kh_id.sql
@@ -0,0 +1,25 @@
+
+-- 멤버들의 아이디와 비밀번호 데이터 삽입 (임의로 생성)
+INSERT INTO MemberCredentials (MNO, Username, Password)
+VALUES
+    (1, 'john_doe', 'password1'),
+    (2, 'jane_smith', 'password2'),
+    (3, 'michael_jones', 'password3'),
+    (4, 'emily_wilson', 'password4'),
+    (5, 'robert_adams', 'password5'),
+    (6, 'sarah_anderson', 'password6'),
+    (7, 'david_williams', 'password7'),
+    (8, 'jennifer_lee', 'password8'),
+    (9, 'william_kim', 'password9'),
+    (10, 'olivia_jeon', 'password10'),
+    (11, 'james_jeong', 'password11'),
+    (12, 'sophia_joo', 'password12'),
+    (13, 'daniel_kim', 'password13'),
+    (14, 'ava_seo', 'password14'),
+    (15, 'matthew_kim', 'password15'),
+    (16, 'emma_shin', 'password16'),
+    (17, 'joseph_lee', 'password17'),
+    (18, 'mia_lim', 'password18'),
+    (19, 'david_kim2', 'password19'),
+    (20, 'ella_lee', 'password20'),
+    (21, 'alexander_kim', 'password21');
